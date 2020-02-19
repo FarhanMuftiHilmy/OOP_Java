1c2aa857cc5308c9b520e41f378d0cc19911df9d
Farhan Mufti Hilmy <farhanmuftihilmy@gmail.com>
2020-02-19 12:35:31 +0700

    Create Latihan3.java
    ini kelas mainnya

---
+++ Latihan3.java
@@ -0,0 +1,31 @@
+class Latihan3{	
+	public static void main(String [] args){
+		/*
+		int panjang = 16;
+		int lebar = 8;
+		String warna1 = "Putih";		
+		*/
+		PapanTulis pp1 = new PapanTulis(16, 8, "Putih");
+				
+		System.out.print(pp1);
+		/*
+		panjang2 = 9;
+		lebar2 = 1;
+		String warna2 = "Biru";
+		*/
+		PapanTulis pp2 = new PapanTulis(9, 7, "Biru");
+		
+		System.out.print(pp2);
+		/*
+		panjang3 = 6;
+		lebar3 = 2;		
+		String warna3 = "Merah";
+		*/
+		PapanTulis pp3 = new PapanTulis(6, 2, "Merah");
+		
+		System.out.print(pp3);
+							
+		
+		
+	}
+}
