bdbd2bb98509cfca2ae0a880db31252c6a17d1b8
Farhan Mufti Hilmy <farhanmuftihilmy@gmail.com>
2020-02-19 12:32:25 +0700

    Create AlatTulis.java
    ini kelas ketiga yang berisi constructor dari Kelas.Java

---
+++ AlatTulis.java
@@ -0,0 +1,12 @@
+class AlatTulis{
+	int harga;
+	int jumlah;
+	String merek;
+	
+	//Constructor
+	public AlatTulis(int harga, int jumlah, String merek){
+		this.harga = harga;
+		this.jumlah = jumlah;
+		this.merek = merek;
+	}	
+}
