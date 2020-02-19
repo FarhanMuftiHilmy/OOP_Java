4a120a472fb508365713043471530d1740088be1
Farhan Mufti Hilmy <farhanmuftihilmy@gmail.com>
2020-02-19 12:31:23 +0700

    Create Elektronik.java
    ini kelas kedua yang berisi constructor dari Kelas.java

---
+++ Elektronik.java
@@ -0,0 +1,12 @@
+class Elektronik{
+	int harga;
+	int jumlah;
+	String kondisi;
+	
+	//Constructor
+	public Elektronik(int harga, int jumlah, String kondisi){
+		this.harga = harga;
+		this.jumlah = jumlah;
+		this.kondisi = kondisi;
+	}
+}
