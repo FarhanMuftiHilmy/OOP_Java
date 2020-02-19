8c535dc9920cca739e2b495529b6da847a0b4cdc
Farhan Mufti Hilmy <farhanmuftihilmy@gmail.com>
2020-02-19 12:30:32 +0700

    Create BendaLain.java
    ini kelas yg berisi constructor dari main Kelas.java

---
+++ BendaLain.java
@@ -0,0 +1,14 @@
+class BendaLain{
+	//Constructor
+	int harga;
+	int jumlah;
+	String jenis;
+	
+	public BendaLain(int name, int harga, int jumlah, String jenis){
+		this.nama = nama;
+		this.harga = harga;
+		this.jumlah = jumlah;
+		this.jenis = jenis;
+	}
+	
+}
