42113338adc2e5933063bb3361d13a9eaf40210d
Farhan Mufti Hilmy <farhanmuftihilmy@gmail.com>
2020-02-26 14:17:56 +0700

    Update Aksi.java

--- Aksi.java
+++ Aksi.java
@@ -5,7 +5,7 @@ public class Aksi{
 	
 	public Aksi(String noPlat){
 		this.noPlat = noPlat;
-		gigi = 0;
+		gigi = 0; 		// gigi dan kecepatan didefinisikan = 0, jadi tidak didefinisikan pada fungsi main
 		kecepatan = 0;
 	}
 	//--------------------------------------//
@@ -28,28 +28,28 @@ public class Aksi{
 		return noPlat;
 	}
 	//-----------------------------//
-	public void Gas(){
+	public void Gas(){ // fungsi untuk menambah 1 kecepatan 
 		kecepatan++;
 	}
 	
-	public void Rem(){
+	public void Rem(){ // fungsi untuk mengurangi 1 kecepatan
 		kecepatan--;
 	}
-	public void NaikkanGigi(){
-		if(kecepatan == 0 && gigi == 4){
+	public void NaikkanGigi(){ //fungsi menaikkan gigi
+		if(kecepatan == 0 && gigi == 4){ // gigi akan = 0 jika kecepatannya 0 dan giginya 4
 			gigi = 0;
-		} else if(kecepatan > 0){
+		} else if(kecepatan > 0){ // jika kecepatan lebih dari 0 gigi akan berubah menjadi 4
 			gigi = 4;
-		}else{
+		}else{ // selain itu gigi akan bertambah 1
 			gigi++;
 		}
 		
 	}
-	public void TurunkanGigi(){
-		if(gigi == 0){
+	public void TurunkanGigi(){ //fungis menurunkan gigi
+		if(gigi == 0){ // gigi akan = 4 jika gigi = 0
 			gigi = 4;
 		}else{
-			gigi--;
+			gigi--; // selain itu gigi bertambah 1
 		}
 		
 	}
