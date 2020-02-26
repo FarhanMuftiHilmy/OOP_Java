0dd500016a834ad39d25c1f2fc355f984940baa2
Farhan Mufti Hilmy <farhanmuftihilmy@gmail.com>
2020-02-26 13:37:31 +0700

    Create Motor.java

---
+++ Motor.java
@@ -0,0 +1,15 @@
+public class Motor{
+	public static void main(String [] args){
+		Aksi bebek = new Aksi("AA1653BB");
+		bebek.Gas();
+		bebek.Gas();
+		bebek.Gas();
+		bebek.Gas();
+		bebek.Gas();
+		bebek.NaikkanGigi();
+		
+		
+		System.out.println(bebek);
+	}
+	
+}
