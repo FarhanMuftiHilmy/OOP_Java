fa8cae38f6085a240423e86fbef49bd327569969
Farhan Mufti Hilmy <farhanmuftihilmy@gmail.com>
2020-02-26 14:17:31 +0700

    Update Motor.java

--- Motor.java
+++ Motor.java
@@ -5,9 +5,15 @@ public class Motor{
 		bebek.Gas();
 		bebek.Gas();
 		bebek.Gas();
+		System.out.println(bebek);	// gas sebanyak 10 kali maka kecepatan akan bertambah 10 dari 0
+		bebek.Gas(); 
 		bebek.Gas();
-		bebek.NaikkanGigi();
-		
+		bebek.Gas();
+		bebek.Gas();
+		bebek.Gas();
+		bebek.Gas();
+		bebek.NaikkanGigi();	//naikkan gigi maka gigi akan bertambah 1
+		bebek.TurunkanGigi(); //turunkan gigi maka gigi akan berkurang 1
 		
 		System.out.println(bebek);
 	}
